ba375f8e33902ef1f51568a9274c31ba7def8a7b
Samuel Ojietohamen <seekersoftec@gmail.com>
2026-02-01 19:59:30 +0100

    fix: Add `suppressHydrationWarning` prop to the body element.

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
